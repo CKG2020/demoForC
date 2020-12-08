4d818d92bace69322ae5ddf11b6404d21df09292
CKG2020 <2488397825@qq.com>
2020-12-08 21:24:59 +0800

    cpp_file

---
+++ test1/testfile1.cpp
@@ -0,0 +1,29 @@
+////
+//// Created by ckg on 20-12-8.
+////
+//
+//#include "testfile1.h"
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
+//int main()
+//{
+//
+//     string s="\"/home/ckg/Desktop/testfile.txt\"";
+//    cout << "this is the begining!" << endl;
+//    int n;
+//    double d;
+//    ofstream outfile;
+//    outfile.open(s, ios::out);
+////    outfile.open("./test.txt", ios::out);
+//    outfile << 50 << endl;
+//    outfile << 3.8 << endl;
+//    outfile.close();
+//    cout << "successful!" << endl;
+////    ifstream infile("/home/ckg/Desktop/testfile.txt", ios::in);
+////    //ifstream infile("./test.txt", ios::in);
+////    infile >> n >> d;
+////    cout << n << "," << d << endl;
+////    infile.close();
+//    return 0;
+//}
---
+++ test1/testfile1.h
@@ -0,0 +1,14 @@
+//
+// Created by ckg on 20-12-8.
+//
+
+#ifndef TEST1_TESTFILE1_H
+#define TEST1_TESTFILE1_H
+
+
+class testfile1 {
+
+};
+
+
+#endif //TEST1_TESTFILE1_H
